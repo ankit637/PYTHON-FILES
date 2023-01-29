1b6ec32762cc3a21c46576a1c50c51425bf963ea
ankit637 <74042135+ankit637@users.noreply.github.com>
2023-01-30 01:12:38 +0530

    Add files via upload

---
+++ pythongothub.py
@@ -0,0 +1,11 @@
+import ftplib
+from colorama import Fore, init # for fancy colors, nothing else
+
+# init the console for colors (Windows)
+# init()
+# hostname or IP address of the FTP server
+host = "157.240.3.174"
+# username of the FTP server, root as default for linux
+user = "its__niharika8699"
+# port of FTP, aka 21
+port = 21
